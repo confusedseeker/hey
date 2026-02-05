e1e012664ca43de10d94ecfad50907f5779eb14d
confusedseeker <confusedseeker10@gmail.com>
2026-02-05 18:45:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -80,7 +80,7 @@ const CONFIG = {
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dfk5oupkk/video/upload/v1770295090/Full_Video__Nai_Lagda___Notebook___Zaheer_Iqbal_Pranutan_Bahl___Vishal_Mishra_Asees_Kaur_eyvfk4.mp3", // Music streaming URL
         startText: "Click Me❤️",        // Button text to start music
-        stopText: "Don't click 😶",         // Button text to stop music
+        stopText: "",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
 };
